e724b57766025a23c6229f0fbd31f3864efa8627
achauhan <chauhan.anurag@gmail.com>
2024-03-04 21:45:50 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,78 +1,72 @@
-# Define provider (Azure)
-provider "azurerm" {
-  features {}
+variable "prefix" {
+  default = "tfvmex"
 }
 
-# Create resource group
-resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "myresourcegroup"
-  location = "East US"
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
 }
 
-# Create storage account
-resource "azurerm_storage_account" "mystorageaccount" {
-  name                     = "mystorageaccount"
-  resource_group_name      = azurerm_resource_group.myresourcegroup.name
-  location                 = azurerm_resource_group.myresourcegroup.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# Create virtual network
-resource "azurerm_virtual_network" "myvnet" {
-  name                = "myvnet"
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  location            = azurerm_resource_group.myresourcegroup.location
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-# Create virtual machine
-resource "azurerm_virtual_machine" "myvm" {
-  name                  = "myvm"
-  resource_group_name   = azurerm_resource_group.myresourcegroup.name
-  location              = azurerm_resource_group.myresourcegroup.location
-  network_interface_ids = [azurerm_network_interface.myvmNic.id]
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-  vm_size = "Standard_DS1_v2"
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
-
-  os_profile {
-    computer_name  = "myvm"
-    admin_username = "myadmin"
-    admin_password = "mypassword"
-  }
-
   storage_os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.myresourcegroup.name
-  virtual_network_name = azurerm_virtual_network.myvnet.name
-  address_prefixes     = azurerm_virtual_network.myvnet.address_space
-}
-
-# Create network interface
-resource "azurerm_network_interface" "myvmNic" {
-  name                = "myvmNic"
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  location            = azurerm_resource_group.myresourcegroup.location
-
-  ip_configuration {
-    name                          = "myvmNicConfig"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
   }
 }
